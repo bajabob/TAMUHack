2186d831a852c03cf27598779079eedfb5bb78d2
Robert Timm <bob@dhcp-10-201-158-223.tamulink.tamu.edu>
2014-10-08 09:06:19 -0500

    adding volunteer page

---
+++ tamuhack_includes/application/controllers/HelpController.php
@@ -0,0 +1,23 @@
+<?php
+
+class HelpController extends Zend_Controller_Action
+{
+
+
+    public function init()
+    {
+    	$this->_helper->layout()->setLayout("org");
+    	
+    	$auth = new Zend_Session_Namespace('Zend_Auth');
+    	if(isset($auth->id))
+    	{
+    		$this->view->auth = $auth;
+    	}
+    }
+
+
+    public function indexAction()
+    {
+    	
+    }
+}
